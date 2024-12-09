f30e5bff09d67252b212a97941b7c37e70626fd1
voltiyo <azizsafouane167@gmail.com>
2024-12-09 17:28:55 +0100

    testing

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const cors = require("cors")
 const app = new express();
 app.use(cors())
 app.use(express.json())
-
+app.enable("trust proxy")
 
 app.get("/",(req,res)=>{
-    res.send(req.headers.host);
+    const data = {
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    }
+    res.send(data);
 })
 
 app.listen(3000)
